c9432bac597381995b056d003633beb1e95ffcae
Willer Almeida Silva <contatowilleralmeida@gmail.com>
2021-09-28 13:20:20 -0300

    teste intellij

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+       gato gato = new gato();
+
+        System.out.println(gato);
+   /* int a = 7;
+    int b = 3;
+
+        System.out.println("OLÁ MUNDOOO" + " " + (a+b));*/
+    }
+}
+
